53d4db04eb2a6070b64366b48693b65cb2d6e76d
sy2123 <sypoppy88@gmail.com>
2023-09-07 12:15:26 -0700

    My NewServerMux

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"github.com/sy2123/RecipeKeeper/data"
 	"github.com/sy2123/RecipeKeeper/handlers"
@@ -9,8 +10,16 @@ import (
 func main() {
 	data.FetchAllRecipes()
 	//fmt.Println(data.AllRecipes)
-	http.HandleFunc("/", handlers.HomePage)
-    http.HandleFunc("/recipe/", handlers.RecipePage)
-	http.ListenAndServe(":8000", nil)
-	
+
+	server := http.NewServeMux()
+
+	style := http.FileServer(http.Dir("frontend/css"))
+
+	server.Handle("/frontend/css/", http.StripPrefix("/frontend/css/", style))
+
+	server.HandleFunc("/", handlers.HomePage)
+	server.HandleFunc("/recipe/", handlers.RecipePage)
+
+	log.Fatal(http.ListenAndServe(":8000", server))
+	    
 }
